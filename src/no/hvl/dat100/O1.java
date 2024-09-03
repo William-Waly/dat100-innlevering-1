c78189ddb8298a121b5cb8016012762a0aec646c
WilliamWaly <184936@stud.hvl.no>
2024-09-03 22:07:06 +0200

    Oppdatert Innlevering 1

--- src/no/hvl/dat100/O1.java
+++ src/no/hvl/dat100/O1.java
@@ -27,3 +27,59 @@ public class O1 {
 		
 	}
 }
+
+// OPPGAVE 2:
+
+package no.hvl.dat100;
+
+import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class O2 {
+
+	public static void main(String[] args) {
+		for(int i=0;i<10;i++) {
+		int tall;
+		do {
+			String tallTxt = showInputDialog("Skriv inn poengsum:");
+			tall = parseInt(tallTxt);
+			if (tall < 0 || tall > 100) {
+				System.out.println("Ugyldig, prøv igjen!");}
+		} while (tall < 0 || tall > 100);
+		if (tall>89) {
+			System.out.println("A");
+		} else if (tall>79) {
+			System.out.println("B");
+		} else if (tall>59) {
+			System.out.println("C");
+		}else if (tall>49) {
+			System.out.println("D");
+		}else if (tall>39) {
+			System.out.println("E");
+		}else {
+			System.out.println("F");}
+		}
+	}
+}
+
+//OPPGAVE 3:
+package no.hvl.dat100;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Integer.*;
+
+public class O3 {
+	
+	public static void main(String[] args) {
+		String tallTxt = showInputDialog("Skriv inn et heltall: ");
+		int n = parseInt(tallTxt);
+		if (n <= 0) {
+            System.out.println("n må være større enn 0.");
+        } else {
+            int fakultet = 1;
+            for (int i = 1; i <= n; i++) {
+                fakultet *= i;
+            System.out.println(i + "! = " + fakultet);
+            }
+        }
+	}
+}
