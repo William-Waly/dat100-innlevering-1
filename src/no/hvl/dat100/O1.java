8dfd9d953e66dfe8caa6f20aaa2c595848095e3b
WilliamWaly <184936@stud.hvl.no>
2024-09-11 20:52:31 +0200

    test

--- src/no/hvl/dat100/O1.java
+++ src/no/hvl/dat100/O1.java
@@ -8,19 +8,19 @@ public class O1 {
 		String tallTxt = showInputDialog("Skriv inn bruttoinntekt: ");
 		int tall = parseInt(tallTxt);
 		if (tall>1350000) {
-			double trinn5 = tall*0.176;
+			double trinn5 = ((tall-1350000)*0.176)+ 68408.6+ 36434.4 + 15086 + 1441.6;
 					System.out.println(trinn5 + "kr skal betales i trinnskatt!");
 		} else if (tall>937900) {
-			double trinn4 = tall*0.166;
+			double trinn4 = ((tall-937900)*0.166)+ 36434.4 + 15086 + 1441.6;
 		System.out.println(trinn4 + "kr skal betales i trinnskatt!");
 		} else if (tall>670000) {
-			double trinn3 = tall*0.136;
+			double trinn3 = ((tall-670000)*0.136)+ 15086 + 1441.6;
 		System.out.println(trinn3 + "kr skal betales i trinnskatt!");
 		}else if (tall>292850) {
-			double trinn2 = tall*0.04;
+			double trinn2 = ((tall-292850)*0.04)+ 1441.6;
 		System.out.println(trinn2 + "kr skal betales i trinnskatt!");
 		}else if (tall>208050) {
-			double trinn1 = tall*0.017;
+			double trinn1 = (tall-208050)*0.017;
 		System.out.println(trinn1 + "kr skal betales i trinnskatt!");
 		}else {
 		System.out.println("Ingen trinnskatt!");}
